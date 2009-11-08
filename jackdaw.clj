42457c841706dd067570130005b38cabdfbc0c0f
Alex Young <alex@helicoid.net>
2009-11-08 21:54:07 +0000

    Started adding "box model" style margin and padding, experimenting with paragraphs

--- jackdaw.clj
+++ jackdaw.clj
@@ -38,7 +38,25 @@
 ; Drawing structs
 (defstruct shape-2d :type :x :y :width :height :style)
 (defstruct text :type :x :y :style :body)
-(defstruct layout :type :width :height)
+(defstruct box :top :right :bottom :left )
+(defstruct layout :type :x :y :width :height :margin :padding)
+
+(def default-box (struct box 10 10 10 10))
+
+(defn default-flow []
+  (ref (struct layout ::Flow 0 0 (@config :width) (@config :height) default-box default-box)))
+
+(defn add-layouts [old new]
+  (struct layout (:type new)
+                 (:x new)
+                 (+ (:y old) (:y new) (:bottom (:margin old)) (:top (:margin new)))
+                 (:width new) (:height new) (:margin new) (:padding new)))
+
+(defn add-height-to-layout [l height]
+  (struct layout (:type l)
+                 (:x l)
+                 height
+                 (:width l) (:height l) (:margin l) (:padding l))) 
 
 ; Drawing
 (defmulti draw (fn [object g l] (:type object)))
@@ -48,53 +66,54 @@
     (.setColor ((r :style) :fill-color))
     (.fillRect (r :x) (r :y) (r :width) (r :height))
     (.setColor ((r :style) :stroke-color))
-    (.drawRect (r :x) (r :y) (r :width) (r :height))))
+    (.drawRect (r :x) (r :y) (r :width) (r :height)))
+  l)
 
 (defmethod draw ::jackdaw/Oval [e g l]
   (doto g
     (.setColor ((e :style) :fill-color))
     (.fillOval (e :x) (e :y) (e :width) (e :height))
     (.setColor ((e :style) :stroke-color))
-    (.drawOval (e :x) (e :y) (e :width) (e :height))))
+    (.drawOval (e :x) (e :y) (e :width) (e :height)))
+  l)
 
 (defmethod draw ::jackdaw/Para [t g l]
-  (doto g
-    (.setRenderingHint (. RenderingHints KEY_ANTIALIASING)
-                       (. RenderingHints VALUE_ANTIALIAS_ON)))
-  (let [body (AttributedString. (t :body))]
-    (doto body
-      (.addAttribute (.. TextAttribute FONT)
-                     (Font. ((t :style) :font) (. Font PLAIN) ((t :style) :size)))
-      (.addAttribute (.. TextAttribute SIZE) ((t :style) :size))
-      (.addAttribute (.. TextAttribute FOREGROUND) ((t :style) :color)))
-    (let [width (l :width)
-          x 10
-          start_y 300
-          ; LineBreakMeasurer does the real work
-          measure (LineBreakMeasurer.
-                  (.. body getIterator)
-                  (.. g getFontRenderContext))]
-      (loop [layout (.. measure (nextLayout width))
-             position (.. measure getPosition)
-             y start_y]
-        (. layout draw g x y)
-        (if (zero? (- (.length (t :body)) position))
-          ()
-          (recur 
-            (.. measure (nextLayout width))
-            (.. measure getPosition)
-            (+ y (. layout getAscent) (. layout getDescent) (. layout getLeading))))))))
-
-(defn default-flow []
-  (ref (struct layout ::Flow (@config :width) (@config :height))))
+  (let [current-layout (ref l)]
+    (doto g
+      (.setRenderingHint (. RenderingHints KEY_ANTIALIASING)
+                         (. RenderingHints VALUE_ANTIALIAS_ON)))
+    (let [body (AttributedString. (t :body))]
+      (doto body
+        (.addAttribute (.. TextAttribute FONT)
+                       (Font. ((t :style) :font) (. Font PLAIN) ((t :style) :size)))
+        (.addAttribute (.. TextAttribute SIZE) ((t :style) :size))
+        (.addAttribute (.. TextAttribute FOREGROUND) ((t :style) :color)))
+      (let [width (- (@current-layout :width) ((@current-layout :padding) :right) ((@current-layout :padding) :left))
+            x (+ ((@current-layout :padding) :left) (@current-layout :x))
+            start_y (+ ((@current-layout :padding) :top) (@current-layout :y))
+            ; LineBreakMeasurer does the real work
+            measure (LineBreakMeasurer.
+                    (.. body getIterator)
+                    (.. g getFontRenderContext))]
+        (loop [text-layout (.. measure (nextLayout width))
+               position (.. measure getPosition)
+               y start_y]
+          (. text-layout draw g x y)
+          (if (zero? (- (.length (t :body)) position))
+            (dosync (ref-set current-layout (assoc @current-layout :y y)))
+            (recur 
+              (.. measure (nextLayout width))
+              (.. measure getPosition)
+              (+ y (. text-layout getAscent) (. text-layout getDescent) (. text-layout getLeading)))))))
+    @current-layout))
 
 (defn draw-all []
   (let [current-layout (default-flow)
         r (doto (proxy [JPanel] [] (paint [g]
     (doall (for [cmd (@config :commands)]
       (if (= ::jackdaw/Stack (cmd :type))
-        (dosync (ref-set current-layout cmd))
-        (draw cmd g @current-layout)))))))]
+        (dosync (ref-set current-layout (add-layouts @current-layout cmd)))
+        (dosync (ref-set current-layout (draw cmd g @current-layout)))))))))]
     (doto (@config :active-frame)
       (.add r)
       (.setVisible true))))
@@ -128,8 +147,16 @@
 (defn para [body]
   (add-cmd (struct text ::Para 10 300 (current-font-style) body)))
 
-(defn stack []
-  (add-cmd (struct layout ::Stack (@config :width) (@config :height))))
+(defn stack
+  ([] (add-cmd (struct layout ::Stack 0 0 (@config :width) (@config :height) default-box default-box)))
+  ([padding] (add-cmd (struct layout ::Stack 0 0 (@config :width) (@config :height) default-box padding))))
 
 (defn flow []
-  (add-cmd (struct layout ::Flow (@config :width) (@config :height))))
+  (add-cmd
+    (struct layout ::Flow 0 0 (@config :width) (@config :height)
+                   default-box default-box)))
+
+(defn padding
+  ([x]                     (struct box x x x x))
+  ([x y]                   (struct box y x y x))
+  ([top right bottom left] (struct box top right bottom left)))
